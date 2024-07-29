ef3b7a64d8416f2b09d63e041cc8155325b7172b
hkneu <hoangnguyen84.neu@gmail.com>
2024-07-29 10:28:01 +0700

    string
    string in python

---
+++ String.py
@@ -0,0 +1,51 @@
+"""
+fist_name = input("nhap ten: ")
+last_name = input("nhap ho: ")
+print("ho ten: " + last_name + (" ") + fist_name)
+"/
+
+"
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural noun: ")
+celebrity = input("Enter a Celebrity: ")
+print("Rose are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+"/
+""
+lucky_number = [8, 7, 9, 2, 1, 3]
+lucky_number.reverse()
+print("day so dao nguoc: ")
+print(lucky_number)
+lucky_number.sort()
+print("day so dc sap xep: ")
+print(lucky_number)
+friends = ["Mike", "Tom", "Peter", "Marry", "Ba"]
+friends.insert(0,"Thu")
+friends.append("Tomy")
+friends.remove("Tom")
+#friends.clear()
+friends.pop()
+#friends.sort()
+print(friends)
+friends2 = friends.copy()
+friends2.sort()
+print("My list friend 2: " + str(friends2))
+"/
+
+"""
+Mon_Convertion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+print(Mon_Convertion["Feb"])
